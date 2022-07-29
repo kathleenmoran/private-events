8557badbb18a1bd46e4ed40510c85cf30a0bef38
Kathleen Moran <kathleenmoran10@gmail.com>
2022-07-29 15:12:09 -0400

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
 end
