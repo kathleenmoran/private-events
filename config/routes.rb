87831456c7a082c345ec64251cef31ecd8e80fab
Kathleen Moran <kathleenmoran10@gmail.com>
2022-07-29 13:51:50 -0400

    Add events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
