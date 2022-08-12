7ef1a63eaa99a872fb16b87ec42ce835a31340af
Kathleen Moran <kathleenmoran10@gmail.com>
2022-08-12 14:04:58 -0400

    Order upcoming events ascending and past events descending

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,10 @@ class Event < ApplicationRecord
   validates :time, presence: true
   validates :location, presence: true, length: { in: 1..50 }
 
-  scope :future, -> { where('time > ?', DateTime.now).order(time: :desc) }
-  scope :past, -> { where('time <= ?', DateTime.now).order(:time) }
-  scope :future_signed_out, -> { where('time > ? AND NOT private', DateTime.now).order(time: :desc) }
-  scope :past_signed_out, -> { where('time <= ? AND NOT private', DateTime.now).order(:time) }
+  scope :future, -> { where('time > ?', DateTime.now).order(:time) }
+  scope :past, -> { where('time <= ?', DateTime.now).order(time: :desc) }
+  scope :future_signed_out, -> { where('time > ? AND NOT private', DateTime.now).order(:time) }
+  scope :past_signed_out, -> { where('time <= ? AND NOT private', DateTime.now).order(time: :desc) }
   scope :not_private, -> { where('NOT private') }
 
   belongs_to :creator, class_name: 'User'
